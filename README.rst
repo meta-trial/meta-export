a287ec035da67bfd6d0f5d8d841f29c8a10a1d5e
erikvw <ew2789@gmail.com>
2019-08-27 21:44:52 +0300

    initial

---
+++ README.rst
@@ -0,0 +1,15 @@
+|pypi| |travis| |coverage|
+
+meta-export
+---------------
+
+Export data from the meta EDC
+
+.. |pypi| image:: https://img.shields.io/pypi/v/meta-export.svg
+    :target: https://pypi.python.org/pypi/meta-export
+    
+.. |travis| image:: https://travis-ci.com/meta-trial/meta-export.svg?branch=develop
+    :target: https://travis-ci.com/meta-trial/meta-export
+    
+.. |coverage| image:: https://coveralls.io/repos/github/meta-trial/meta-export/badge.svg?branch=develop
+    :target: https://coveralls.io/github/meta-trial/meta-export?branch=develop
---
+++ runtests.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import django
+import logging
+import os
+import sys
+
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+from edc_test_utils import DefaultTestSettings
+from os.path import abspath, dirname, join
+
+
+app_name = 'meta_export'
+base_dir = dirname(abspath(__file__))
+
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    SUBJECT_VISIT_MODEL="meta_subject.subjectvisit",
+    EDC_BOOTSTRAP=3,
+    EMAIL_BACKEND="django.core.mail.backends.locmem.EmailBackend",
+    EMAIL_CONTACTS={
+        "data_request": "someone@example.com",
+        "data_manager": "someone@example.com",
+    },
+    EMAIL_ENABLED=True,
+    HOLIDAY_FILE=join(base_dir, app_name, "tests", "holidays.csv"),
+    LIVE_SYSTEM=False,
+    RANDOMIZATION_LIST_PATH=join(
+        base_dir, app_name, "tests", "test_randomization_list.csv"),
+    INSTALLED_APPS=[
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.sites',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'django_crypto_fields.apps.AppConfig',
+        'django_revision.apps.AppConfig',
+        'edc_registration.apps.AppConfig',
+        'meta_export.apps.AppConfig',
+    ],
+    add_dashboard_middleware=True,
+    add_lab_dashboard_middleware=True,
+    # use_test_urls=True,
+).settings
+
+
+def main():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    django.setup()
+    tags = [t.split('=')[1] for t in sys.argv if t.startswith('--tag')]
+    failures = DiscoverRunner(failfast=True, tags=tags).run_tests(
+        [f'{app_name}.tests'])
+    sys.exit(failures)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
